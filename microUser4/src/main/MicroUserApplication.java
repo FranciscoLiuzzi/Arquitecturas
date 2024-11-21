a230581ab25af02fd0e67e2ce3a73c568b841af4
FranciscoLiuzzi <lliuzzietchelet@alumnos.exa.unicen.edu.ar>
2024-11-20 22:56:34 -0300

    Arrancan

--- microUser4/src/main/MicroUserApplication.java
+++ microUser4/src/main/MicroUserApplication.java
@@ -3,7 +3,7 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"main.Repositories", "main.Services", "main.Objects", "main.DTOs"})
 public class MicroUserApplication {
 	public static void main (String[] args) {
 		SpringApplication.run(MicroUserApplication.class, args);
